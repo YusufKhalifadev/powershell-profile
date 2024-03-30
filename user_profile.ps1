3333cfb233f738e6925ad52809a3faa529da64e5
YusufKhalifadev <yusufkhalifadev@gmail.com>
2024-03-30 10:18:15 +0200

    initial

---
+++ user_profile.ps1
@@ -0,0 +1,128 @@
+# YusufKhlaifa's powershell (based on christitustech at "github.com/ChrisTitusTech/powershell-profile/")
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+function prompt { 
+    if ($isAdmin) {
+        "[" + (Get-Location) + "] # " 
+    } else {
+        "[" + (Get-Location) + "] $ "
+    }
+}
+
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+    } else {
+        Start-Process "$psHome\powershell.exe" -Verb runAs
+    }
+}
+
+# Alias
+#
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
+Set-Alias -Name vim -Value nvim
+
+function rm($file) {
+    Remove-Item -Force -Recurse $file
+}
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+function pgrep($name) {
+    Get-Process $name
+}
+
+function ll { Get-ChildItem -Path $pwd }
+function g { Set-Location G:\Github }
+function gcom {
+    git add .
+    git commit -m "$args"
+}
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
+}
+function Get-PubIP {
+    (Invoke-WebRequest http://ifconfig.me/ip ).Content
+}
+function uptime {
+    #Windows Powershell only
+	If ($PSVersionTable.PSVersion.Major -eq 5 ) {
+		Get-WmiObject win32_operatingsystem |
+        Select-Object @{EXPRESSION={ $_.ConverttoDateTime($_.lastbootuptime)}} | Format-Table -HideTableHeaders
+	} Else {
+        net statistics workstation | Select-String "since" | foreach-object {$_.ToString().Replace('Statistics since ', '')}
+    }
+}
+
+function reload-profile {
+    & $profile
+}
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+function ix ($file) {
+    curl.exe -F "f:1=@$file" ix.io
+}
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
+function df {
+    get-volume
+}
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+ 
+# zoxide (autocompletion)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+## Final Line to set prompt
+oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
